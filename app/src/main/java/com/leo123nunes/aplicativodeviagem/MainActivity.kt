316f6a913bfab5f35b7cb0fef157bda18d581956
leo123nunes <ti.userleo@hotmail.com>
2020-09-19 10:35:09 -0300

    Adicionando evento para o botão Calcular e validando entrada de dados

--- app/src/main/java/com/leo123nunes/aplicativodeviagem/MainActivity.kt
+++ app/src/main/java/com/leo123nunes/aplicativodeviagem/MainActivity.kt
@@ -2,10 +2,43 @@ package com.leo123nunes.aplicativodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        if(id==R.id.buttonCalculate){
+            calculate()
+        }
+    }
+
+    private fun calculate(){
+        if(validateInput()){
+            val distance = editDistance.text.toString().toFloat()
+            val price = editPrice.text.toString().toFloat()
+            val autonomy = editAutonomy.text.toString().toFloat()
+
+            val total = (distance*price)/autonomy
+
+            totalPrice.text = "R$ ${"%.2f".format(total)}"
+        }
+    }
+
+    private fun validateInput(): Boolean{
+        return editDistance.text.toString()!=""&&
+                editPrice.text.toString()!=""&&
+                editAutonomy.text.toString()!=""
     }
 }
+
+
+
